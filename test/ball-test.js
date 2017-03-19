48b2f19a95f175186882290a857a96fd54658ade
Jennifer Peavler <jenn.peavler@gmail.com>
2017-03-19 16:11:03 -0600

    Add win level and game over functionality.  Set up testing suite

---
+++ test/ball-test.js
@@ -0,0 +1,10 @@
+var assert = require('chai').assert;
+var Ball = require('../lib/ball');
+
+describe('Ball', function () {
+
+  it('should be a function', function () {
+    assert.isFunction(Ball);
+  });
+
+}
