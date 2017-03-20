095b4762b7abd9545f3aedd4ab8a76d295bb6fa8
Jack Bevis <jack.bevis@gmail.com>
2017-03-20 16:54:46 -0600

    adding files from staging for git pull

---
+++ test/test-ball.js
@@ -0,0 +1,12 @@
+var assert = require('chai').assert;
+var Ball = require('../lib/ball');
+
+//describe ball
+
+//it should be a function
+
+//it should be an object
+
+//it should have an x and y
+
+//it should move
---
+++ test/test-bricks.js
@@ -0,0 +1,12 @@
+var assert = require('chai').assert
+var Bricks = require('../lib/bricks')
+
+//describe bricks
+
+//it should be function
+
+//it should be an object
+
+//it should have an x and y
+
+//they're should be an array of bricks???
