a451e4b25ca8e367307c6d327791d46c92e7724b
Jack Bevis <jack.bevis@gmail.com>
2017-03-20 18:03:46 -0600

    add paddle test suite, and started es6 refactor of ball and bricks test suite

--- test/ball-test.js
+++ test/ball-test.js
@@ -7,81 +7,62 @@ var board = {
 }
 
 describe('Ball', function () {
+  let ball = new Ball(board, 10, 80);
 
-  it('should be a function', function () {
+  it('should be a function', () => {
     assert.isFunction(Ball);
   });
 
-  it('the Ball function should be able to construct an object', function () {
-    var ball = new Ball(board, 10, 80);
+  it('the Ball function should be able to construct an object', () => {
     assert.isObject(ball);
   });
 
-  it('should have a radius of 5' ,function() {
-    var ball = new Ball(board, 10, 80);
+  it('should have a radius of 5' , () => {
     assert.equal(ball.radius, 5);
   });
 
-  it('should have a property with a key named x', function() {
-    var ball = new Ball(board, 10, 80);
+  it('should have a property with a key named x', () => {
     assert.property(ball, 'x');
   });
 
-  it('should have a property with a key named y', function() {
-    var ball = new Ball(board, 10, 80);
+  it('should have a property with a key named y', () => {
     assert.property(ball, 'y');
   });
 
-  it('should begin at an x position that is half the width of the canvas', function() {
-    var ball = new Ball(board, 10, 80);
+  it('should begin at an x position that is half the width of the canvas', () => {
     assert.equal(ball.x, board.canvas.width/2);
   });
 
-  it('should know the paddle width and height', function() {
-    var ball = new Ball(board, 10, 80);
+  it('should know the paddle width and height',() => {
     assert.property(ball, 'paddleHeight');
     assert.property(ball, 'paddleWidth');
   });
 
-  it('should begin at a y position that is the canvas height minus the paddle height minus the ball radius', function() {
-    var ball = new Ball(board, 10, 80);
+  it('should begin at a y position that is the canvas height minus the paddle height minus the ball radius', () => {
     assert.equal(ball.y, board.canvas.height - ball.paddleHeight - ball.radius);
   });
 
-  it('should begin with an x velocity of 4 and a y velocity of -4', function() {
-    var ball = new Ball(board, 10, 80);
+  it('should begin with an x velocity of 4 and a y velocity of -4', () => {
     assert.equal(ball.dx, 4);
     assert.equal(ball.dy, -4);
   });
 
-  it('should be able to be drawn', function() {
-    var ball = new Ball(board, 10, 80);
+  it('should be able to be drawn', () => {
     assert.property(ball, 'draw');
   });
 
-  it('should move', function() {
-    var ball = new Ball(board, 10, 80);
+  it('should move', () => {
     assert.property(ball, 'move');
   });
 
-  it('changes x when it moves', function() {
-    // var obj = { val: 10 };
-    // var fn = function() { obj.val = 22 };
-    // assert.changes(fn, obj, 'val');
-    var ball = new Ball(board, 10, 80);
-    // assert.changes(move, ball, 'ball.x');
+  it('changes x when it moves', () => {
     ball.move();
-    assert.notEqual(ball.x, 4);
+    assert.notEqual(ball.x, board.canvas.width/2);
   });
 
-  it('changes y when it moves', function() {
-    // var obj = { val: 10 };
-    // var fn = function() { obj.val = 22 };
-    // assert.changes(fn, obj, 'val');
-    var ball = new Ball(board, 10, 80);
-    // assert.changes(move, ball, 'ball.x');
+  it('changes y when it moves', () => {
     ball.move();
-    assert.notEqual(ball.y, -4);
+    assert.notEqual(ball.y, board.canvas.height - this.paddleHeight - this.radius);
   });
 
 });
---
+++ test/paddle-test.js
@@ -0,0 +1,56 @@
+var assert = require('chai').assert;
+var Paddle = require('../lib/paddle');
+
+describe('Paddle', () => {
+  const board = {
+                  canvas: {width: 520, height: 500},
+                  context: context
+                }
+  const paddle = new Paddle(board);
+
+  it('should be a function', () => {
+    assert.isFunction(Paddle);
+  });
+
+  it('should construct an object', () => {
+    assert.isObject(paddle);
+  });
+
+  it('should have attributes', () => {
+    assert.property(paddle, 'x');
+    assert.property(paddle, 'y');
+    assert.property(paddle, 'width');
+    assert.property(paddle, 'height');
+    assert.property(paddle, 'moveRight');
+    assert.property(paddle, 'moveLeft');
+  });
+
+  it('should be able to be drawn to the canvas', () => {
+    assert.property(paddle, 'draw');
+    assert.isFunction(paddle.draw);
+  });
+
+  it('should not move left if paddle.moveLeft is false', () => {
+    assert.equal(paddle.moveLeft, false);
+    paddle.move();
+    assert.equal(paddle.x, board.canvas.width/2 - paddle.width/2)
+  });
+
+  it('should not move right if paddle.moveRight is false', () => {
+    assert.equal(paddle.moveRight, false);
+    paddle.move();
+    assert.equal(paddle.x, board.canvas.width/2 - paddle.width/2)
+  });
+
+  it('should move left if paddle.moveLeft is true', () => {
+    paddle.moveLeft = true;
+    paddle.move();
+    assert.notEqual(paddle.x, board.canvas.width/2 - paddle.width/2)
+  });
+
+  it('should move right if paddle.moveRight is true', () => {
+    paddle.moveRight = true;
+    paddle.move();
+    assert.notEqual(paddle.x, board.canvas.width/2 - paddle.width/2);
+  });
+})
